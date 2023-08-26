53a33755a0e52fde5d8c5184a617d40a5faf2a5a
parvj <parvjain2401@gmail.com>
2023-08-26 18:27:44 +0530

    history

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+
 const port = 3001;
 
 const calculation = require("./routes/calculate");
+const history = require("./routes/history");
 
-
+app.use("/history",history);
 app.use("/",calculation);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
