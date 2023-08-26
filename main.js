43de155e42026c616710fd83ccaabf2800741855
parvj <parvjain2401@gmail.com>
2023-08-26 14:02:11 +0530

    endpoint

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const port = 3001;
 
 const calculation = require("./routes/calculate");
 
-app.use("/:values*",calculation);
+
+app.use("/",calculation);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- routes/calculate.js
+++ routes/calculate.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/",(req,res) =>{
+
+router.get("/", (req, res) => {
+  const content = `
+  <body>
+    <h2> Welcome </h2>
+    <p>Send Mathematical Operation in URL</p>
+    
+    <p>Methods to write Expression:</p>
+    <ul>
+        <li>plus: + 
+        <p>Example: <a href="http://localhost:3001/5/plus/2">http://localhost:3001/5/plus/2</a></p>
+        </li>
+        <li>minus: -
+        <p>Example: <a href="http://localhost:3001/5/minus/2">http://localhost:3001/5/minus/2</a></p>
+        </li>
+        <li>into: *
+        <p>Example: <a href="http://localhost:3001/5/into/2">http://localhost:3001/5/into/2</a></p>
+        </li>
+        <li>by: /
+        <p>Example: <a href="http://localhost:3001/5/by/2">http://localhost:3001/5/by/2</a></p>
+        </li>
+    </ul>
+    <br>
+    <p>Check History: <a href="/history">History</a></p>
+  </body>
+  `;
+  res.send(content);
+});
+
+
+router.get("/:values*",(req,res) =>{
     const values = req.params.values + req.params[0];
   const parts = values.split('/');
 
@@ -56,7 +86,7 @@ router.get("/",(req,res) =>{
     }
   }
 
-  res.send(`question:"${expression}",answer:${total}`);
+  res.send(`question:"${expression}" , answer:${total}`);
 })
 
 module.exports = router;
